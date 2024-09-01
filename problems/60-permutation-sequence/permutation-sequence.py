f5c31fefd63129594babaafe9ea0b5e20819af32
Prashansa Jain <123321339+prashansaaa29@users.noreply.github.com>
2024-09-01 22:26:26 +0530

    Time: 12 ms (80.04%) | Memory: 11.5 MB (95.72%) - LeetSync

--- problems/60-permutation-sequence/permutation-sequence.py
+++ problems/60-permutation-sequence/permutation-sequence.py
@@ -1,15 +1,15 @@
 import math
 class Solution(object):
     def getPermutation(self, n, k):
-        numbers=range(1,n+1) 
-        permutation=''
+        numbers=range(1,n+1)
+        p=''
         k=k-1
         while n>0:
             n=n-1
-            index,k=divmod(k,math.factorial(n))
-            permutation+=str(numbers[index])
-            numbers.remove(numbers[index])
-        return permutation
+            i,k=divmod(k,math.factorial(n))
+            p=p+str(numbers[i])
+            numbers.remove(numbers[i])
+        return p
         
 
         
